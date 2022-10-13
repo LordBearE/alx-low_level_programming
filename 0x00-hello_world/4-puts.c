1c3f9377819fa2556ed09df7e42707cee7494115
LordBearE <chidiogwo@yahoo.com>
2022-10-13 14:23:59 -0700

    task 4a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
 /**
+ *main - Entry point
  *
- * *main - Entry point
- *
- * *Return: Always 0 (Success)
- *
- * */
-
-
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
 
 {
@@ -19,5 +13,3 @@ int main(void)
 					return (0);
 
 }
-
-
